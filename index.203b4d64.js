ff1a9cf68048aa22d7cf98f652088f5431ccfaea
Matt B <mbaranich@yahoo.com>
2021-07-02 00:05:03 -0400

    Initial commit of existing project.

---
+++ index.203b4d64.js
@@ -0,0 +1,2 @@
+const collapsibles=document.querySelectorAll(".collapsible");collapsibles.forEach((l=>l.addEventListener("click",(function(){this.classList.toggle("collapsible--expanded")}))));
+//# sourceMappingURL=index.203b4d64.js.map
